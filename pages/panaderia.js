fcfc4d3fffc05eef8be1206aa6db336ec03129df
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-21 16:36:47 -0400

    1 file modified

--- pages/panaderia.js
+++ pages/panaderia.js
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from '../styles/panaderia.module.css';
-import logo from "../utils/logoEmpresarial.png"
+import logo from "../utils/television.png"
 export default function panaderia(){
 return(
   <div>
