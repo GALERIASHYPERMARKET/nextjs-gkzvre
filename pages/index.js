e6458fa51ae7f26a1676c753d75145eb94ad2514
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-21 15:14:56 -0400

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import Image from 'next/image'
 import Header from "../components/Header";
 
 export default function Home() {
+  
   return (
     <div >
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header/>
+      <img src="../utils/logoEmpresarial.png" alt="" />
     </div>
   );
 }
