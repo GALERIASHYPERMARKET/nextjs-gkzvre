2a8bee15c9c1b77668f3e54f592063b24c351fd7
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-21 14:53:36 -0400

    1 file modified

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,9 +17,9 @@ export default function Header() {
       <nav style={{ display: "flex", flexDirection: "row",backgroundColor:"Green", justifyContent: "space-between"  }}>
         {navLinks?.map((link, index) => {
           return (
-            <ul style={{listStyleType: "none",paddingLeft:"5px" }}>
+            <ul style={{listStyleType: "none",paddingLeft:"5px", backgroundColor:"black" }}>
               <Link href={link.path}>
-                <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{backgroundColor: isHover ? 'lightblue' : 'rgb(0, 191, 255)', fontFamily:"Verdana, Arial, Helvetica, sans-serif",color:"white",  }}  key={index}>{link.name}</li>
+                <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{backgroundColor:isHover?"black": "blue", fontFamily:"Verdana, Arial, Helvetica, sans-serif",color:"white",  }}  key={index}>{link.name}</li>
               </Link>
             </ul>
           );
