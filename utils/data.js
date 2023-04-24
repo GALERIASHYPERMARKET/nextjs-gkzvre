0be8720cf90089265fa1b92a1c660c98706771ec
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-24 16:46:04 -0400

    Add files via upload

--- utils/data.js
+++ utils/data.js
@@ -1,37 +1,47 @@
-
-
 export const navLinks = [
-  { name: "Gourmet", 
-   path: "/gourmet" 
-  },
-  {
-    name: "Charcuteria",
-    path: "/charcuteria",
-  },
-  {
-    name: "Carniceria",
-    path: "/carniceria",
-  },
+    { name: "Gourmet", 
+     path: "/gourmet" 
+    },
+    {
+      name: "Charcuteria",
+      path: "/charcuteria",
+    },
+    {
+      name: "Carniceria",
+      path: "/carniceria",
+    },
+    
+    {
+      name:"Panaderia",
+      path: "/panaderia"
+    },
+    {
+        name:"Pollera",
+        path: "/panaderia"
+      },
+      {
+        name:"Jugos",
+        path: "/panaderia"
+      },
+      {
+        name:"Desayunos",
+        path: "/panaderia"
+      },
+    
   
-  {
-    name:"Panaderia",
-    path: "/panaderia"
-  },
+  ];
   
-
-];
-
-export const articulosd=[
+  export const articulosd=[
+    { descripcion: "carne de primera",
+    precio: "7,58" 
+   },
+   { descripcion: "carne de primera",
+   precio: "7,58" 
+  },
   { descripcion: "carne de primera",
   precio: "7,58" 
- },
- { descripcion: "carne de primera",
- precio: "7,58" 
-},
-{ descripcion: "carne de primera",
-precio: "7,58" 
-},
-{ descripcion: "carne de primera",
-  precio: "7,58" 
- },
-];
+  },
+  { descripcion: "carne de primera",
+    precio: "7,58" 
+   },
+  ];
