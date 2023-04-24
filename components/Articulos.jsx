283d4a50e069695430f640a866b42fff8f5121ef
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-24 16:39:56 -0400

    2 files modified

--- components/Articulos.jsx
+++ components/Articulos.jsx
@@ -1,16 +1,17 @@
 const Articulos = ({ data }) => {
   return (
-    <div >
+    <div className="flex flex-col space-y-8 text-5xl text-white  w-3/5">
       {data?.map((item) => (
-        <div  >
-          <p >
+        <div  className=" flex flex-row pt-12 justify-between">
+          <div className="font-bold  text-left ">
             {item.descripcion}
-          </p>
-          <p >
-            {item.precio}
-          </p>
-          <p></p>
+          </div>
           
+          <div className="bg-green-500 font-bold rounded-full text-center ">
+            {item.precio}
+          </div>
+         
+
         </div>
       ))}
     </div>
--- pages/carniceria.js
+++ pages/carniceria.js
@@ -1,6 +1,6 @@
-import Articulos from "../components/Articulos.jsx"
+import Articulos from "../components/Articulos"
 import { useEffect, useState } from "react";
-import {supabase}  from "../lib/initSupabase.js";
+import supabase  from "../lib/supabase.js";
 import {articulosd} from "../utils/data.js"
 export default function carniceria(){
   //aqui se conectara a la base de datos
@@ -10,47 +10,52 @@ export default function carniceria(){
   
   
   useEffect(() => {
+    
     cargarArticulos();
   }, []);
 
   const cargarArticulos = async () => {
-    console.log(supabase);
+    
     try {
       
       const { data, error } = await supabase
-        .from("ARTICULO")
-        .select("*")
+        .from("articulo")
+        .select("descripcion , precio").eq("id_departamento",1).range(0,3)
  
       if (error) throw error;
       setarticulos(data);
-      
     } catch (error) {
       alert(error.message);
     } finally {
       
     }
+    
   };
-  const cargarA=()=>{
-    setarticulos(articulosd);
-  }
+  
   
   const numerodeArticulos = async (id_departamento) =>{
      try{
       const { count, error } = await supabase
-  .from('ARTICULOS')
+  .from('articulo')
   .select('*', { count: 'exact', head: true })
   .eq("id_departamento",id_departamento)
   if (error) throw error;
-  setlimite(count);
-   return count;
+     setlimite(count);
+  
      }catch (error) {
       alert(error.message);
     } 
   }
 
   return (
-    <>
-      <Articulos data={articulos}/>
-    </>
+    <div className=" p-6 mx-0 bg-charcuteria bg-cover h-screen w-screen flex flex-col items-center space-y-12">
+        <div className="flex flex-col space-y-6 items-center">
+          <h1 className="font-bold text-5xl text-white">Carniceria</h1>
+          <h3 className="font-bold text-5xl text-white">Productos para ti</h3>
+        </div>
+        
+         <Articulos data={articulos}/>
+    </div>
+     
   )
 }
