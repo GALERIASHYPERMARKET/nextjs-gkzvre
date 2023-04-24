03c001e2bdd58b19fb5e72a2a2f09b9f1a427870
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-24 17:00:00 -0400

    2 files modified

---
+++ pages/gourmet.js
@@ -0,0 +1,13 @@
+export default function gourmet(){
+  return (
+    <div className=" p-6 mx-0 bg-charcuteria bg-cover h-screen w-screen flex flex-col items-center space-y-12">
+        <div className="flex flex-col space-y-6 items-center">
+          <h1 className="font-bold text-5xl text-white">Carniceria</h1>
+          <h3 className="font-bold text-5xl text-white">Productos para ti</h3>
+        </div>
+        
+         
+    </div>
+     
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
   // Or if using `src` directory:
   "./src/**/*.{js,ts,jsx,tsx,mdx}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'charcuteria': 'url("../utils/carniceria.jpg")',
+      },
+    },
   },
   plugins: [],
 }
