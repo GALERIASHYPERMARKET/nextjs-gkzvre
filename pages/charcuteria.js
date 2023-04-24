493edf1e334160ea237baf0cab8c051496490330
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-24 17:05:09 -0400

    1 file modified

--- pages/charcuteria.js
+++ pages/charcuteria.js
@@ -1,53 +1,7 @@
 import Articulos from "../components/Articulos.jsx"
 import { useEffect, useState } from "react";
 function charcuteria(){
-  const [articulos, setarticulos] = useState([]);
-  const [contador, setcontador]=useState(0);
-  const [limite, setlimite]=useState(0);
   
-  numerodeArticulos(1);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      
-      cargarArticulos(1,contador);
-      setcontador(contador+1);
-      if(contador==limite-4){
-        setcontador(0)
-      }
-     },60*1000);
-  }, []);
-
-  const cargarArticulos = async (id_departamento,limite) => {
-    
-    try {
-      
-      const { data, error } = await supabase
-        .from("ARTICULO")
-        .select("descripcion, id_departamento, precio")
-        .eq("id_departamento", id_departamento).range(limite, limite+3);
- 
-      if (error) throw error;
-      setarticulos(data);
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      
-    }
-  };
-
-  const numerodeArticulos = async (id_departamento) =>{
-     try{
-      const { count, error } = await supabase
-  .from('ARTICULOS')
-  .select('*', { count: 'exact', head: true })
-  .eq("id_departamento",id_departamento)
-  if (error) throw error;
-  setlimite(count);
-   return count;
-     }catch (error) {
-      alert(error.message);
-    } 
-  }
 
   return (
     <>
